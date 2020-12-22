104e482328dea6570d6aa36ceacbd5d87fdf79fa
Sim Melinda <ymsimy@yahoo.com>
2020-12-22 16:56:55 +0100

    mobile->képernyő commit

--- Script/js/FenyoFa2.js
+++ Script/js/FenyoFa2.js
@@ -7,9 +7,9 @@ var angle = 0, step = (2*Math.PI) / fields.length;
 fields.each(function() {
     var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
     var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
-    if(window.console) {
+    /*if(window.console) {
         console.log($(this).find("img"), x, y);
-    }
+    }*/
 	$(this).parent().css({position: 'relative'});
     $(this).css({
         left: x + 'px',
