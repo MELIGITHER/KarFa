e0598d89f6df7b49755866b95e0278c70c918eaa
Sim Melinda <ymsimy@yahoo.com>
2021-01-05 19:51:21 +0100

    ind.jav commit

--- indKarFa.js
+++ indKarFa.js
@@ -3,32 +3,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-const nodemailer = require("nodemailer");
 
 var iso88592 = require('iso-8859-2');
-//const utf8 = require('utf8');
-
-app.use(express.static(__dirname + '/Script'));
+//__dirname : project folder.
+console.log(__dirname);
 //Store all JS and CSS in Scripts folder.In css/images- all images
+app.use(express.static(path.join(__dirname,'Script')));
 
-
-
-
-console.log(path.dirname);
-
-app.use(express.static(__dirname + '/Pages'));
 //Store all HTML files in Pages folder.
+app.use(express.static(path.join(__dirname,'Pages')));
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/Pages/indKarFa.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/Pages/about.html'));
+  res.sendFile(path.join(__dirname,'Pages/indKarFa.html'));
 });
 
 app.use('/', router);
-app.listen(process.env.PORT || 3000 ,function(){
-    console.log("up and running on port "+process.env.PORT);
+const port=process.env.PORT || 3000;
+app.listen(port ,function(){
+    console.log('Up and running on port:'+port);
 });
