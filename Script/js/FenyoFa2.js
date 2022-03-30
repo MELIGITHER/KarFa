00f15d2b5c0d3da2b1476bbeefa85ca35d008191
Sim Melinda <ymsimy@yahoo.com>
2022-03-30 15:23:38 +0200

    rewrite commit

--- Script/js/FenyoFa2.js
+++ Script/js/FenyoFa2.js
@@ -1,21 +1,31 @@
 function run2() {
-var radius = 200;
-var fields = $('.item'), container = $('#kep-container'), width = container.width(), height = container.height();
-var angle = 0, step = (2*Math.PI) / fields.length;
-/*console.log(width);
+  var fields = $(".item"),
+    container = $(".kep-container"),
+    width = container.width(),
+    height = container.height();
+
+  if (width == 600) var radius = 200;
+  else var radius = 150;
+  var angle = 0,
+    step = (2 * Math.PI) / fields.length;
+  /*console.log(width);
 console.log(height);*/
-fields.each(function() {
-    var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
-    var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
+  fields.each(function () {
+    var x = Math.round(
+      width / 2 + radius * Math.cos(angle) - $(this).width() / 2
+    );
+    var y = Math.round(
+      height / 2 + radius * Math.sin(angle) - $(this).height() / 2
+    );
     /*if(window.console) {
         console.log($(this).find("img"), x, y);
     }*/
-	$(this).parent().css({position: 'relative'});
+    $(this).parent().css({ position: "relative" });
     $(this).css({
-        left: x + 'px',
-        top: y + 'px', 
-		position:'absolute'
+      left: x + "px",
+      top: y + "px",
+      position: "absolute",
     });
     angle += step;
-});
+  });
 }
